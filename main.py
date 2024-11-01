629734bd513919ecb1cc956a31b9710a364bdb6c
Philippe Hiroux <12002903@student.pxl.be>
2024-11-01 21:53:56 +0100

    Add logo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import messagebox
 import subprocess  # Om een ander Python-script aan te roepen
+from PIL import Image, ImageTk  # Voor het werken met JPG-afbeeldingen
 
 def open_city_window():
     # Start het script voor het toevoegen van een stad
@@ -11,22 +12,35 @@ def open_year_window():
     subprocess.Popen(['python', 'year.py'])
 
 def open_map_window():
-    # Start het script voor het toevoegen van een jaar
+    # Start het script voor het tonen van een map
     subprocess.Popen(['python', 'map.py'])
 
 # Hoofdvenster instellen
 root = tk.Tk()
 root.title("Hoofdmenu")
 
-# Creëer knoppen voor stad toevoegen en jaar toevoegen
-add_city_button = tk.Button(root, text="Stad Toevoegen", command=open_city_window, width=30)
-add_city_button.pack(pady=20)
+# Schaal de afbeeldingen
+city_image = ImageTk.PhotoImage(Image.open("image/city.jpg").resize((100, 100)))
+year_image = ImageTk.PhotoImage(Image.open("image/Year.jpg").resize((100, 100)))
+map_image = ImageTk.PhotoImage(Image.open("image/Map.jpg").resize((100, 100)))
 
-add_year_button = tk.Button(root, text="Jaar Toevoegen", command=open_year_window, width=30)
-add_year_button.pack(pady=20)
+# Maak een frame voor de knoppen
+button_frame = tk.Frame(root)
+button_frame.pack(pady=20)  # Voeg wat ruimte boven het frame
 
-add_year_button = tk.Button(root, text="Map tonen", command=open_map_window, width=30)
-add_year_button.pack(pady=20)
+# Creëer knoppen met afbeeldingen en plaats ze in het frame
+add_city_button = tk.Button(button_frame, image=city_image, command=open_city_window)
+add_city_button.grid(row=0, column=0, padx=10)  # Voeg wat ruimte tussen de knoppen
+
+add_year_button = tk.Button(button_frame, image=year_image, command=open_year_window)
+add_year_button.grid(row=0, column=1, padx=10)  # Voeg wat ruimte tussen de knoppen
+
+add_map_button = tk.Button(button_frame, image=map_image, command=open_map_window)
+add_map_button.grid(row=0, column=2, padx=10)  # Voeg wat ruimte tussen de knoppen
+
+# Voeg een label toe onder de knoppen
+footer_label = tk.Label(root, text="Made by Philippe-Arnaud Hiroux ©", font=("Arial", 10))
+footer_label.pack(side=tk.BOTTOM, pady=10)  # Plaats het label onderaan met wat ruimte
 
 # Start de Tkinter event loop
 root.mainloop()
