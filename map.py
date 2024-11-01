db81a1c26ab95621b474fb48e9193f9b9dae6d8f
Philippe Hiroux <12002903@student.pxl.be>
2024-11-01 19:39:04 +0100

    Improt data in data.py file

--- map.py
+++ map.py
@@ -1,27 +1,8 @@
 import plotly.express as px
 import pandas as pd
+from data import import_data_from_file
 
-def import_data_from_file(filename):
-    data_list = []
-    
-    with open(filename, 'r') as file:
-        for line in file:
-            line = line.strip()  # Verwijder witruimtes aan het begin en einde van de regel
-            if line:  # Alleen niet-lege regels verwerken
-                try:
-                    # Verander de string naar een dictionary door gebruik te maken van eval
-                    data = eval(line)  # Gebruik eval() hier
-                    if isinstance(data, dict):  # Verifieer dat het een dictionary is
-                        data_list.append(data)
-                    else:
-                        print(f"Regel is geen dictionary: {line}")
-                except (SyntaxError, ValueError) as e:
-                    print(f"Fout bij het verwerken van regel: {line}. Foutmelding: {e}")
-                    
-    return data_list
-
-filename = 'location.txt'
-cities = import_data_from_file(filename)
+cities = import_data_from_file()
 print(cities)
 
 # Zet de data om in een DataFrame
